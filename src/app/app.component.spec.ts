b2466d48dca8d2fc7b0ad9393ac8cbed0eec9ee5
Riccardo Andreatta <riccardo.andreatta@hotmail.com>
2019-08-14 14:43:18 +0100

    basic Angular 5 app

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,32 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+
+describe('AppComponent', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppComponent]
+        }).compileComponents();
+    }));
+
+    it('should create the app', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
+
+    it(`should have the same content as found in the environment file`, async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app.APP_NAME).toEqual(environment.appName);
+    }));
+
+    it('should render title in a h1 tag', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h1').textContent).toContain(
+            'I am using an environment file written at built time'
+        );
+    }));
+});
